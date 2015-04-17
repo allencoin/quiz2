b6fba0ee5346487274f09c3e1508b095dc1214d1
Allen Coin <allencoin@gmail.com>
2015-04-17 04:48:39 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quiz2::Application.config.secret_key_base = 'add984dcc131a78494bb24bc3ce6f3fe507e9c8b3aa9fcbb5ea468721e8f9f400578a8c8940493a150dfc4c4ed5a77e0b83a02b95e3eb7415dd88c180ad10a5b'
