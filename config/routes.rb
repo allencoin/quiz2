1a8313e914987befed54b0dc4b488b4397c74fb2
Allen Coin <allencoin@gmail.com>
2015-04-17 05:03:51 +0000

    add database columns

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Quiz2::Application.routes.draw do
+  root 'comments#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
